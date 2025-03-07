2faba7a7f2908bcf5ccdb2682d70a50e72ed2dd6
gaurav kumar <gauravku8485@gmail.com>
2025-03-07 12:46:48 +0530

    my commit

---
+++ src/OnlinePaymentSystem.java
@@ -0,0 +1,25 @@
+interface UPIPayment {
+    void upiTransfer();
+}
+
+interface CreditCardPayment {
+    void cardTransfer();
+}
+
+class PaymentGateway implements UPIPayment, CreditCardPayment {
+    public void upiTransfer() {
+        System.out.println("UPI Payment Successful");
+    }
+
+    public void cardTransfer() {
+        System.out.println("Credit Card Payment Successful");
+    }
+}
+
+public class OnlinePaymentSystem {
+    public static void main(String[] args) {
+        PaymentGateway transaction = new PaymentGateway();
+        transaction.upiTransfer();
+        transaction.cardTransfer();
+    }
+}
